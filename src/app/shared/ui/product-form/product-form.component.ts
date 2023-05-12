cbf12c44849ffba302a2c29461fe0d3fe9e1f8f0
MyStic <khanh.votuan@hcmut.edu.vn>
2023-05-12 23:49:56 +0700

    init product form

---
+++ src/app/shared/ui/product-form/product-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-form',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>Product form work</p>`,
+})
+export class ProductFormComponent {}
